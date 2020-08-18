5ecef8ec2162335f1db6b58323b5c8c67d9ba2c6
dylanribau@gmail.com <dylanribau@gmail.com>
2020-08-18 04:10:13 -0400

    Can view the formatted JSON for available creatures in the creatures page

--- src/app/creatures/creatures.component.ts
+++ src/app/creatures/creatures.component.ts
@@ -8,6 +8,7 @@ import { CreaturesService } from './creatures.service';
 })
 export class CreaturesComponent implements OnInit {
   allCreatures = [];
+  currentCreature: string = "";
 
   constructor(private creaturesService: CreaturesService) { }
 
@@ -17,5 +18,12 @@ export class CreaturesComponent implements OnInit {
         this.allCreatures.push(value.creature);
       };
     });
+  };
+
+  displayCreature(creature){
+    //this.currentCreature = JSON.stringify(creature, null, 4);
+    //this.currentCreature = prettyPrintJson.toHtml(creature);
+    this.currentCreature = prettyPrintJson.toHtml(creature);
+    console.log(this.currentCreature);
   };
 }
