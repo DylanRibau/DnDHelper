af7fd4f3ddd85869679b6e91a2741b0fe2ea5aab
dylanribau@gmail.com <dylanribau@gmail.com>
2020-09-08 15:06:21 -0400

    Created a landing/home page

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  changePage(page){
+    this.router.navigateByUrl('/' + page);
   }
 
 }
