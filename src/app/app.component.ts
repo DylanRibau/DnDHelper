b89d4025c1b7dc92b6a87aecb5511152348c919d
dylanribau@gmail.com <dylanribau@gmail.com>
2020-12-21 13:07:28 -0500

    Added the user's username to the logout button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,31 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   isLoggedIn: boolean;
+  loggedUsername: string;
 
   constructor(private authUtil: AuthUtil, private router: Router){
     router.events.subscribe(result => {
       this.checkLoggedIn();
     });
-  }
+  };
 
   ngOnInit():void {
     this.checkLoggedIn();
-  }
+  };
 
   checkLoggedIn() {
     this.isLoggedIn = this.authUtil.isLoggedIn();
-  }
+    if(this.isLoggedIn)
+      this.getLoggedInUsername();
+  };
+
+  getLoggedInUsername(){
+    this.loggedUsername = this.authUtil.getUsername();
+  };
 
   logout() {
     this.authUtil.doLogoutUser();
     this.isLoggedIn = false;
     this.router.navigate(['/index']);
-  }
+  };
 }
