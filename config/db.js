828045beb53777be9c23885d57907aef3bfade67
dylanribau@gmail.com <dylanribau@gmail.com>
2020-12-22 15:53:24 -0500

    Adding files for heroku deployment

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: 'mongodb://localhost:27017/DnDHelper',
+  prod: 'mongodb+srv://DnDHelper:guuZi64RZK2WSLr@dndhelper.azuvu.mongodb.net/DnDHelper?retryWrites=true&w=majority'
+}
